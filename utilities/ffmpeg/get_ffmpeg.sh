629f8bb84658574bdc61197e951570fbeb694426
rhradec <me@hradec.com>
2022-02-07 16:38:05 -0800

    added git snapshot tars for x264 and ffmpeg

--- utilities/ffmpeg/get_ffmpeg.sh
+++ utilities/ffmpeg/get_ffmpeg.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-if [ -d ffmpeg ]; then
-   cd ffmpeg
-   git pull
-   cd ..
-else
-	git clone git://source.ffmpeg.org/ffmpeg.git ffmpeg
-fi
 
+if [ -e ffmpeg.tar.gz ] ; then
+    tar xvf ffmpeg.tar.gz
+else
+    if [ -d ffmpeg ]; then
+        cd ffmpeg
+        git pull
+        cd ..
+    else
+        git clone --depth=1 git://source.ffmpeg.org/ffmpeg.git ffmpeg
+        tar czf ffmpeg.tar.gz ./ffmpeg
+    fi
+fi
--- utilities/ffmpeg/get_x264.sh
+++ utilities/ffmpeg/get_x264.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-if [ -d x264 ]; then
-   cd x264
-   git pull -v
-   cd ..
+if [ -e x264.tar.gz ] ; then
+        tar xvf x264.tar.gz
 else
-   git clone https://code.videolan.org/videolan/x264.git
+    if [ -d x264 ]; then
+        cd x264
+        git pull -v
+        cd ..
+    else
+        git clone --depth=1 https://code.videolan.org/videolan/x264.git
+        tar czf x264.tar.gz ./x264
+  fi
 fi
