97b778f4ed61db31d77472e1b755f8d6d404843a
timurhai <timurhai@gmail.com>
2024-10-30 10:38:05 +0300

    FATAL_ERROR task state.
    Errors automatic restart settings will be ignored.
    This state can be set by parser `fatalerror` property.
    
    Closes: #602.

--- afanasy/python/parsers/parser.py
+++ afanasy/python/parsers/parser.py
@@ -24,6 +24,7 @@ class parser(object):
         self.frame = 0
         self.percentframe = 0
         self.error = False
+        self.fatalerror = False
         self.warning = False
         self.badresult = False
         self.finishedsuccess = False
@@ -40,6 +41,8 @@ class parser(object):
         if not hasattr(self, 'str_warning'):
             self.str_warning = []
         if not hasattr(self, 'str_error'):
+            self.str_error = []
+        if not hasattr(self, 'str_fatalerror'):
             self.str_error = []
         if not hasattr(self, 'str_badresult'):
             self.str_badresult = []
@@ -125,6 +128,9 @@ class parser(object):
         for string in self.str_error:
             if lower.find(string.lower()) != -1:
                 self.error = True
+        for string in self.str_fatalerror:
+            if lower.find(string.lower()) != -1:
+                self.fatalerror = True
         for string in self.str_badresult:
             if lower.find(string.lower()) != -1:
                 self.badresult = True
